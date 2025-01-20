9d610902d8b3bbc6f8e3145a1e8f48169625d7f4
Slava <slavik3001@rambler.ru>
2025-01-20 14:19:32 +0300

    Deleted comments

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -4,32 +4,21 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
 from alembic import context
 
+from config import settings
 from database.models import Base, Users # noqa
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-from config import settings
+
 config.set_main_option("sqlalchemy.url", str(settings.postgres_url))
+
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
