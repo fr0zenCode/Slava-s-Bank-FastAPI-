95fd4bc1448127d91e6ad68a40f46884a75cbf7d
Slava <slavik3001@rambler.ru>
2025-01-21 17:53:54 +0300

    Realize more methods

--- app/repositories/users/postgres.py
+++ app/repositories/users/postgres.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
 from datetime import date
 
-from sqlalchemy import insert, update
+from pydantic import EmailStr
+from sqlalchemy import insert, update, select
 
 from .abstract import AbstractUsersRepository
 from database.models import Users
 from database.core import get_sqlalchemy_async_database_helper
-from .schemas import AddUserSchema, UserID, SuccessfulMessageJSON, UnsuccessfulMessageJSON
+from .schemas import AddUserSchema, UserID, SuccessfulMessageJSON, UnsuccessfulMessageJSON, UserSchema, PhoneNumber
 
 
 @dataclass
@@ -39,14 +40,33 @@ class SQLAlchemyPostgresUsersRepository(AbstractUsersRepository):
             await session.commit()
             return SuccessfulMessageJSON()
 
-    async def get_user_by_id(self):
-        ...
+    async def activate_user_by_id(self, user_id: UserID) -> SuccessfulMessageJSON | UnsuccessfulMessageJSON:
+        async with self.session_factory() as session:
+            stmt = update(Users).values(is_active=True).where(Users.id == user_id.value)
+            await session.execute(stmt)
+            await session.commit()
+            return SuccessfulMessageJSON()
 
-    async def get_user_by_email(self):
-        ...
+    async def get_user_by_id(self, user_id: UserID) -> UserSchema:
+        async with self.session_factory() as session:
+            stmt = select(Users).where(Users.id == user_id.value)
+            user = await session.execute(stmt)
+            user_as_pydantic_model = user.scalar_one().convert_to_pydantic_model()
+            return user_as_pydantic_model
 
-    async def get_user_by_phone(self):
-        ...
+    async def get_user_by_email(self, email: EmailStr) -> UserSchema:
+        async with self.session_factory() as session:
+            stmt = select(Users).where(Users.email == email)
+            user = await session.execute(stmt)
+            user_as_pydantic_model = user.scalar_one().convert_to_pydantic_model()
+            return user_as_pydantic_model
+
+    async def get_user_by_phone(self, phone: PhoneNumber) -> UserSchema:
+        async with self.session_factory() as session:
+            stmt = select(Users).where(Users.phone == phone.value)
+            user = await session.execute(stmt)
+            user_as_pydantic_model = user.scalar_one().convert_to_pydantic_model()
+            return user_as_pydantic_model
 
 
 def get_sqlalchemy_postgres_users_repository() -> SQLAlchemyPostgresUsersRepository:
