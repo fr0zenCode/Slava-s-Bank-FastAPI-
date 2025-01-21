931f68637ebecb5ee31060fd01a23fe4e835216b
Slava <slavik3001@rambler.ru>
2025-01-21 14:44:21 +0300

    Add endpoint for deactivation user

--- app/endpoints/users.py
+++ app/endpoints/users.py
@@ -2,7 +2,8 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 
-from repositories.users.schemas import AddUserFromEndpointsSchema
+from repositories.users.schemas import AddUserFromEndpointsSchema, UserID, SuccessfulMessageJSON, \
+    UnsuccessfulMessageJSON
 from services.users.service import UsersService, get_users_service
 
 users_router = APIRouter(prefix="/users", tags=["Users API"])
@@ -23,3 +24,12 @@ async def create_user(
         "successful": True,
         "created-user-id": new_user_id.value
     }
+
+
+@users_router.post("/deactivate-user")
+async def deactivate_user(
+        user_id: UserID,
+        users_service: Annotated[UsersService, Depends(get_users_service)]
+) -> SuccessfulMessageJSON | UnsuccessfulMessageJSON:
+    json_response_as_pydantic_model = await users_service.deactivate_user_by_id(user_id=user_id)
+    return json_response_as_pydantic_model
