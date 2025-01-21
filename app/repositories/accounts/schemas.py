f2f384e2bd830b0c42991381a2c18599075e64a4
Slava <slavik3001@rambler.ru>
2025-01-21 18:01:46 +0300

    Initial commit

---
+++ app/repositories/accounts/schemas.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+from database.enums import AccountStatus
+
+
+class AddAccountFromEndpointsSchema(BaseModel):
+    handler_id: str
+    balance: float
+    account_status: AccountStatus
+
+
+class AddAccountSchema(AddAccountFromEndpointsSchema):
+    id: str
+    inspiration_date: date
+    creation_date: date
+
+
+class AccountID(BaseModel):
+    value: str
