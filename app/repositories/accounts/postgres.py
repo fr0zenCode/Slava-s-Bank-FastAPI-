cf96d375e6ddf91b4afac2ee4d5d6c6e7c6a05aa
Slava <slavik3001@rambler.ru>
2025-01-21 18:04:01 +0300

    Realize some methods for accounts repository based on postgres and sqlalchemy

--- app/repositories/accounts/postgres.py
+++ app/repositories/accounts/postgres.py
@@ -1,15 +1,37 @@
 from dataclasses import dataclass
 
+from sqlalchemy import insert
+
+from database.models import Accounts
+from database.core import get_sqlalchemy_async_database_helper
 from .abstract import AbstractAccountsRepository
+from .schemas import AddAccountSchema, AccountID
 
 
 @dataclass
 class SQLAlchemyPostgresAccountRepository(AbstractAccountsRepository):
 
-    async def create_account(self):
-        ...
+    database_helper = get_sqlalchemy_async_database_helper()
+    session_factory = database_helper.get_session_factory()
+
+    async def create_account(self, account: AddAccountSchema) -> AccountID:
+        async with self.session_factory() as session:
+            stmt = insert(Accounts).values(
+                id=account.id,
+                handler_id=account.handler_id,
+                creation_date=account.creation_date,
+                inspiration_date=account.inspiration_date,
+                balance=account.balance,
+                account_status=account.account_status
+            ).returning(Accounts.id)
+            response = await session.execute(stmt)
+            await session.commit()
+            return AccountID(value=response.scalar_one())
 
     async def deactivate_account(self):
+        ...
+
+    async def activate_account(self):
         ...
 
     async def get_account_by_id(self):
@@ -23,3 +45,7 @@ class SQLAlchemyPostgresAccountRepository(AbstractAccountsRepository):
 
     async def decrease_balance(self):
         ...
+
+
+def get_sqlalchemy_postgres_accounts_repository() -> SQLAlchemyPostgresAccountRepository:
+    return SQLAlchemyPostgresAccountRepository()
