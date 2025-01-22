ed116f40d0263a16ec971a9f2e19013e6290efc5
Slava <slavik3001@rambler.ru>
2025-01-22 03:30:35 +0300

    Rename class name. Expend methods signatures

--- app/repositories/transactions/abstract.py
+++ app/repositories/transactions/abstract.py
@@ -1,17 +1,20 @@
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 
+from repositories.transactions.schemas import TransactionID, AddTransactionSchema, TransactionSchema
+from repositories.users.schemas import UserID
+
 
 @dataclass
-class AbstractOperationsRepository(ABC):
+class AbstractTransactionsRepository(ABC):
     @abstractmethod
-    async def registry_transaction(self, from_person, to_person):
+    async def registry_transaction_in_system(self, transaction: AddTransactionSchema) -> TransactionID:
         ...
 
     @abstractmethod
-    async def get_transaction_by_id(self):
+    async def get_transaction_by_id(self, transaction_id: TransactionID) -> TransactionSchema:
         ...
 
     @abstractmethod
-    async def get_all_users_transactions_by_user_id(self):
+    async def get_all_users_transactions_by_user_id(self, user_id: UserID) -> list[TransactionSchema]:
         ...
