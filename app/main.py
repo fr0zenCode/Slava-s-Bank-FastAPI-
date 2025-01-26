474b42ff9550e85d2205fc7614b77e1afb5fe03c
Slava <slavik3001@rambler.ru>
2025-01-26 05:29:07 +0300

    Include transactions router

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from endpoints.accounts import accounts_router
+from endpoints.transactions import transactions_router
 from endpoints.users import users_router
 
 app = FastAPI()
 app.include_router(users_router)
 app.include_router(accounts_router)
+app.include_router(transactions_router)
 
 
 if __name__ == '__main__':
